36cf9c7d6e168440fe6605954a81026334cacf46
kanna <s4020232@rmit.edu.vn>
2025-12-29 00:12:20 +0700

    feat: add MovieNotFoundException

---
+++ src/main/java/org/example/appmovies/exception/MovieNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.appmovies.exception;
+
+public class MovieNotFoundException extends RuntimeException {
+    public MovieNotFoundException(Long id) {
+        super("Movie not found with id: " + id);
+    }
+}
