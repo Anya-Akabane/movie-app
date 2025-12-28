abb09521ba5f3d718926f69ab3ed0c1e48ac78b9
kanna <s4020232@rmit.edu.vn>
2025-12-29 00:06:45 +0700

    feat: add GET /movies endpoint

--- src/main/java/org/example/appmovies/controller/MovieController.java
+++ src/main/java/org/example/appmovies/controller/MovieController.java
@@ -2,10 +2,9 @@ package org.example.appmovies.controller;
 
 import org.example.appmovies.model.Movie;
 import org.example.appmovies.service.MovieService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
@@ -20,5 +19,10 @@ public class MovieController {
     @PostMapping
     public Movie createMovie(@RequestBody Movie movie) {
         return service.createMovie(movie);
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return service.getAllMovies();
     }
 }
