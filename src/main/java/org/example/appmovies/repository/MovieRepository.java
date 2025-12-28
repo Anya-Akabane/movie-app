04e37733e84009230886ea720e091eb0f764ff08
kanna <s4020232@rmit.edu.vn>
2025-12-28 21:59:01 +0700

    feat: implement MovieRepository

---
+++ src/main/java/org/example/appmovies/repository/MovieRepository.java
@@ -0,0 +1,8 @@
+package org.example.appmovies.repository;
+
+import org.example.appmovies.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
+
