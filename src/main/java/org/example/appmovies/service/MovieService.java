d2917c78c475f26ab62164e9e9ec8b5bfe2e6ee5
kanna <s4020232@rmit.edu.vn>
2025-12-28 23:59:30 +0700

    feat: add getAllMovies to MovieService interface

--- src/main/java/org/example/appmovies/service/MovieService.java
+++ src/main/java/org/example/appmovies/service/MovieService.java
@@ -2,6 +2,10 @@ package org.example.appmovies.service;
 
 import org.example.appmovies.model.Movie;
 
+import java.util.List;
+
 public interface MovieService {
     Movie createMovie(Movie movie);
+    List<Movie> getAllMovies();
+
 }
