86e53ba1c36278e198770e4e96bfbc381b079456
kanna <s4020232@rmit.edu.vn>
2025-12-28 14:31:32 +0700

    some message

---
+++ src/test/java/org/example/appmovies/AppMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.appmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
